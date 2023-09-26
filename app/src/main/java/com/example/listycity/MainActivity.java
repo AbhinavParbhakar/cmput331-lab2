d74884ada68704c65f6b6d81b9b475506110dd90
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-09-26 14:36:29 -0600

    Implemented the changes for adding to a list

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.listycity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,11 +18,17 @@ public class MainActivity extends AppCompatActivity {
     ListView city;
     ArrayList<String> cityList;
     ArrayAdapter<String> cityAdaper;
+    Button addButton;
+    Button deleteButton;
+    EditText editText;
+    Button addEntry;
+
+    LinearLayout inputDiv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String [] cities = {"Delhi","this thing","that thing"};
+        String [] cities = {"Delhi","New Mexico","Bear"};
         cityList = new ArrayList<String>();
 
         city = findViewById(R.id.city_list);
@@ -27,6 +37,39 @@ public class MainActivity extends AppCompatActivity {
         cityAdaper = new ArrayAdapter<>(this,R.layout.activity_main,cityList);
         city.setAdapter(cityAdaper);
 
+
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        addButton = findViewById(R.id.add_button);
+        deleteButton = findViewById(R.id.delete_button);
+        editText = findViewById(R.id.edit_text);
+        addEntry = findViewById(R.id.add_entry);
+        inputDiv = findViewById(R.id.input_div);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputDiv.setVisibility(View.VISIBLE);
+            }
+        });
+
+        addEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cityLabel = editText.getText().toString();
+                if (cityLabel.length() == 0){
+                    inputDiv.setVisibility(View.INVISIBLE);
+                }else{
+                    cityList.add(cityLabel);
+                    cityAdaper.notifyDataSetChanged();
+                    inputDiv.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
 
     }
